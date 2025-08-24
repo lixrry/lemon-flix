154f10c5c326dea16afff198b7ed1230cfb31b58
lixrry <pierenandrew@outlook.com>
2025-08-24 17:43:36 -0400

    Update source.ts

--- src/stores/player/slices/source.ts
+++ src/stores/player/slices/source.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-import { ScrapeMedia } from "@p-stream/providers";
+import { ScrapeMedia } from "@lixrry/lemon-providers";
 
 import { MakeSlice } from "@/stores/player/slices/types";
 import {
