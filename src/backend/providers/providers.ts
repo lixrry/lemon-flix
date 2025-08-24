81662e22ef62cb05d7bcf569ff65fa23ca580e81
lixrry <pierenandrew@outlook.com>
2025-08-24 16:29:59 -0400

    Change name and sources (#1)
    * Delete src/assets/locales directory
    
    * Add files via upload
    
    * Delete src/pages directory
    
    * Add files via upload
    
    * Delete src/backend directory
    
    * Add files via upload
    
    * Update opensearch.xml.hbs
    
    * Update package.json
    
    * Update manifest.json
    
    * Update index.html
    
    * Update pnpm-lock.yaml

--- src/backend/providers/providers.ts
+++ src/backend/providers/providers.ts
@@ -2,7 +2,7 @@ import {
   makeProviders,
   makeStandardFetcher,
   targets,
-} from "@p-stream/providers";
+} from "@lixrry/lemon-providers";
 
 import { isExtensionActiveCached } from "@/backend/extension/messaging";
 import {
--- src/pages/PlayerView.tsx
+++ src/pages/PlayerView.tsx
@@ -1,4 +1,4 @@
-import { RunOutput } from "@p-stream/providers";
+import { RunOutput } from "@lixrry/lemon-providers";
 import { useCallback, useEffect, useRef, useState } from "react";
 import {
   Navigate,
